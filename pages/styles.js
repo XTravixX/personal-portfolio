aaa42dd4fdd1e8dc1f674f196155d88232423aa5
Ricardo Vivas <travix212@hotmail.com>
2021-11-21 14:53:11 -0400

    add selection color

--- pages/styles.js
+++ pages/styles.js
@@ -1,7 +1,9 @@
-import styled from 'styled-components';
-
-
+import styled from "styled-components"
 
 export const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
   padding: 25px;
 `
