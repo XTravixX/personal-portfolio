e50e015274e71cc839b4a475216c9be651369710
Ricardo Vivas <travix212@hotmail.com>
2021-11-21 15:22:29 -0400

    add Description on homepage

--- pages/styles.js
+++ pages/styles.js
@@ -7,3 +7,26 @@ export const StyledMain = styled.main`
   justify-content: center;
   padding: 25px;
 `
+
+export const StyledLanguage = styled.div`
+  margin-left: auto;
+  width: 35px;
+  height: 35px;
+  border-radius: 35px;
+  padding-bottom: 3px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  background: var(--primary-color);
+  color: var(--text-default-invert);
+  outline: none;
+  user-select: none;
+
+`
+export const StyledDescription = styled.p`
+  font-size: 1.4rem;
+  font-weight: 300;
+  text-align: center;
+  margin: 25px 0px;
+`
